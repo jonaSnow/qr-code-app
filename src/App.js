5228a9e7c1ee9381591168c2fa22e218d69bf644
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2020-02-29 22:37:22 -0500

    initial commit for qr-form-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={ title: "Form 1",
+    question1: "What is your name?",
+    question2: "Are you a baby?",
+    id: 1,
+    answer1: "",
+    answer2: "" };
+    }
+    
+  onAnswer1Change = e => {
+    const answer1 = e.target.value;
+    this.setState(() => ({ answer1 }));
+  };
+
+  onAnswer2Change = e => {
+    const answer2 = e.target.value;
+    this.setState(() => ({ answer2 }));
+  };
+  
+  handleSubmit = e =>{
+    e.preventDefault();
+    const url ="https://jsonplaceholder.typicode.com/posts"
+    const data = { answer1:this.state.answer1, answer2:this.state.answer2 }
+    fetch(url, { method: "POST",
+      body: JSON.stringify(data),
+      headers:{ "Content-type": "application/json" } })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response)); 
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label>
+            {this.state.question1}
+            <input
+              type="text"
+              placeholder="Answer"
+              autoFocus
+              onChange={this.onAnswer1Change}
+            />
+          </label>
+
+          <label>
+            {this.state.question2}
+            <input
+              type="text"
+              placeholder="Answer"
+              autoFocus
+              onChange={this.onAnswer2Change}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
